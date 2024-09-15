a6f35ce0a186f6afc93e183a31740e85638db59a
MattMaester <maesterbarbosa@gmail.com>
2024-09-14 21:08:24 -0300

    added base file structure; api test; environmental file

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,9 @@
+
+export async function GET(request) {
+  return new Response(JSON.stringify({ message: 'Hello, World!' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
