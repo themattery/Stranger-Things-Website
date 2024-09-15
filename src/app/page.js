5d1b826bb39ac21f8f33439c74e84e37cb573092
MattMaester <maesterbarbosa@gmail.com>
2024-09-15 19:30:08 -0300

    react test; rendering lists; responding to events; hooks

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,60 @@
+"use client";
+import { useState } from 'react';
+
+const user = {
+  name: "Harry Potter",
+  imageUrl: "",
+  age: 90,
+};
+
+const products = [
+  {title: 'Order of Phoenix', id: 5},
+  {title: 'Sorcerer\'s Stone', id: 1},
+  {title: 'Halfblood Prince', id: 6},
+];
+
+const productsList = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+)
+
+function Button() {
+  const [count, setCount] = useState(0);
+
+  function messageAlert() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={messageAlert}>
+      Click {count} times before!
+    </button>
+  );
+}
+
+function MyImage() {
+  return (
+    <img class="img" src="https://picsum.photos/200/200"/>
+  );
+}
+
+function MyList() {
+  return (
+    <ul>
+      {productsList}
+    </ul>
+  );
+}
 
 export default function HomePage() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to the Home Page</h1>
+      <MyImage />
+      <Button />
+      <MyList />
+      <Button />
+    </>
+    );
 }
