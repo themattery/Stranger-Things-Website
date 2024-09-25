5446b54704167cb2344fb180b213ce33f4d79f83
Matheus Barbosa <115056575+themattery@users.noreply.github.com>
2024-09-25 20:13:57 -0300

    github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/Stranger-Things-Website",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
